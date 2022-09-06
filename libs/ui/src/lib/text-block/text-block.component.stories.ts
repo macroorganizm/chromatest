79c3de4bac19e7213551688f2b2940f434568c6a
oleh rybnikov <valor-team@valor-teams-Mac-mini.local>
2022-09-06 15:17:00 +0300

    added storybook

---
+++ libs/ui/src/lib/text-block/text-block.component.stories.ts
@@ -0,0 +1,35 @@
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { TextBlockComponent } from './text-block.component';
+
+export default {
+  title: 'TextBlockComponent',
+  component: TextBlockComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [],
+    }),
+  ],
+} as Meta<TextBlockComponent>;
+
+const Template: Story<TextBlockComponent> = (args: TextBlockComponent) => ({
+  template: `
+    <olchromatic-text-block [bordered]="bordered">
+      <div text-block-header>
+        <h2>Some header</h2>
+      </div>
+      <p style="color: blue">Content Start</p>
+      <p>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <p>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <p style="color: blue">Content end</p>
+      <div text-block-footer>
+        <p>Some footer</p>
+      </div>
+    </olchromatic-text-block>
+`,
+  props: args,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  bordered: false,
+};
