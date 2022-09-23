bc947262a96756c82b82ca84a23c02810adad797
oleh rybnikov <valor-team@valor-teams-Mac-mini.local>
2022-09-23 13:33:22 +0300

    change button 2-1

--- libs/ch-uilib/button-new/src/lib/button.component.stories.ts
+++ libs/ch-uilib/button-new/src/lib/button.component.stories.ts
@@ -17,6 +17,6 @@ const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
 
 export const Primary = Template.bind({});
 Primary.args = {
-  text: 'button label',
-  isRed: true,
+  text: 'button label 123',
+  isRed: false,
 };
