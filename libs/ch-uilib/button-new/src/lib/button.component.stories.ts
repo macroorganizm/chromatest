58599101dbe12715877acc9f368826f4cb71d571
Oleg Rybnikov <oleg.rybnikov@valor-software.com>
2022-09-30 14:00:59 +0300

    remove only stories flag (#43)
    * remove only stories flag
    
    * buttom ch
    
    Co-authored-by: oleh rybnikov <valor-team@valor-teams-Mac-mini.local>

--- libs/ch-uilib/button-new/src/lib/button.component.stories.ts
+++ libs/ch-uilib/button-new/src/lib/button.component.stories.ts
@@ -17,6 +17,6 @@ const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
 
 export const Primary = Template.bind({});
 Primary.args = {
-  text: 'button label',
+  text: 'button label 123412',
   isRed: false,
 };
